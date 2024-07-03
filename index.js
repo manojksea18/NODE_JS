d1450a1e71678a9c6fdfd1ca5c1898fb0bac0273
Manoj Khati <mksea18@gmail.com>
2024-07-03 12:51:34 +0545

    index.js updated

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const http = require("http");
 const os = require("os");
+const fs = require("fs");
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-
-    //calculate your machine ram using os
-    const myRam = os.totalmem().toFixed(2);
-    res.end(
-      JSON.stringify({
-        data: myRam,
-      })
-    );
+    // res.writeHead(200, { "Content-Type": "application/json" });
+    // //calculate your machine ram using os
+    // const myRam = os.totalmem().toFixed(2);
+    // res.end(
+    //   JSON.stringify({
+    //     data: myRam,
+    //   })
+    // );
   })
   .listen(3000);
+
+fs.readFile("hello.txt", "utf8", (error, data) => {
+  console.log(data);
+  // console.log(error);
+});
